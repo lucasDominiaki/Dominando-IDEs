fc90e4c9f6843bcb4c628b779070bd4ce2a2c7ef
Luchie <lucasdominiaki@gmail.com>
2021-11-27 18:40:14 -0300

    Dicas

--- primeiro programa.js
+++ primeiro programa.js
@@ -1,6 +1,20 @@
+/*ECLIPSE*/
+
 package com.dio;
 
 public=class primeiroPrograma{
     public static void main(String[] args)
     system.out.printIn("!!!!Hello World!!!!!");
 }
+
+/*  Dica
+CTRL+SHIFT+F = REORGANIZA PARA OS PADROES NORMAIS.
+CONTROL+M = TERMINA UMA FRASE JÁ EXISTENTE PARA POUPAR TEMPO.
+CTRL+O = FAZ A IMPORTAÇÃO DE UMA CLASSE.
+CTRL+2+L = CRIA UMA VARIAVEL ATRAVÉS DA PROPRIA IDE.
+CTRL+ALT= COPIA O MESMO CODIGO PARA LINHAS ABAIXO.
+
+
+ DEBUG OU DEPURACAO.
+
+- Debugar um codigo é encontrar um erro em um codigo para resolver o problema.
