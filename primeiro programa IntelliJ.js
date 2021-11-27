a6cfcbc541c52c7617310fdc63e2d35a39af96cd
Luchie <lucasdominiaki@gmail.com>
2021-11-27 19:58:47 -0300

    Outro commit

--- primeiro programa IntelliJ.js
+++ primeiro programa IntelliJ.js
@@ -37,6 +37,5 @@ Alt+Enter = faz o import
 CTRL+O = Apaga os imports que não estão ah ser utilizados.
 CTRL+Y = Deleta a linha. */ 
 
-
-/* Versionamento
-
+/* Plugins
+Rainbown Brackets = Muda a cor da chaves, pode auxiliar na hora, caso você se perca.
