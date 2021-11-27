81bc09e35b530e85df9528c0c3cf3d9cb1e19e04
Luchie <lucasdominiaki@gmail.com>
2021-11-27 18:51:29 -0300

    mais algumas dicas

--- primeiro programa.js
+++ primeiro programa.js
@@ -7,7 +7,7 @@ public=class primeiroPrograma{
     system.out.printIn("!!!!Hello World!!!!!");
 }
 
-/*  Dica
+/*  Dica no ecliplse
 CTRL+SHIFT+F = REORGANIZA PARA OS PADROES NORMAIS.
 CONTROL+M = TERMINA UMA FRASE JÁ EXISTENTE PARA POUPAR TEMPO.
 CTRL+O = FAZ A IMPORTAÇÃO DE UMA CLASSE.
@@ -18,3 +18,13 @@ CTRL+ALT= COPIA O MESMO CODIGO PARA LINHAS ABAIXO.
  DEBUG OU DEPURACAO.
 
 - Debugar um codigo é encontrar um erro em um codigo para resolver o problema.
+
+Break Point, após inserir o ponto ao qual o programa ser executado,o programa ira rodar até certo porto, depois ira perguntar se você ira querer mudar de perspectiva
+para a do debug.
+
+F5= IRA PULAR PARA O METODO PARA QUAL VOCÊ ACHAR QUE ESTÁ ERRADO.
+F7= RETORNA PARA A LINHA AH QUAL VOCÊ ESTAVA NO COMEÇO DA APURAÇÃO.
+
+-LEMBRANDO QUE O 'INT' SÃO NUMERO INTEIROS, ELE NAO IRA MOSTRAR NÚMEROS COMO 2.5, PARA ISSO TERA QUE USAR O FLOAT
+ISSO NO CASO DE UMA DIVISÃO.
+F6= PULA A LINHA
