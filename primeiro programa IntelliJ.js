3a372a6c12fb0ef21006f35edc8f5c8497246b68
Luchie <lucasdominiaki@gmail.com>
2021-11-27 19:17:19 -0300

    Apenas commit mesmo

--- primeiro programa IntelliJ.js
+++ primeiro programa IntelliJ.js
@@ -4,4 +4,39 @@ public class Primeiroprograma {
     public static void main(String[] args){
         FileSystem.out.printIn("Hello World");
     }
+};
+
+
+public class gato{
+    private String nome ;
+    private String  cor ;
+    private Integer idade;
+
+    public Gato() {}
+
+    public Gato (String nome, String cor, Integer idade){
+        this.nome = nome;
+        this.cor = cor;
+        this.idade = idade;
+    }
 }
+
+
+
+
+
+/* Dicas
+digitar sout ira ajudar a imprimir o 'print' mais rapidamente.
+CRTL+SHIFT+F10=  Ira iniciar o programa e mostrar o metodo main.
+Após isso apenas precisara apertar SHIFT + F10 para iniciar o programa, pois o programa já foi configurado.
+ALT+INSERT= ira te levar para  a janela de geração, la você podera criar equal and hash codes, e mais umas coisas
+que vão facilitar na hora de codar.
+CTRL+SHIFT+/= ele comenta o bloco selecionado.
+CTRL + / = apenas a linha.
+Alt+Enter = faz o import
+CTRL+O = Apaga os imports que não estão ah ser utilizados.
+CTRL+Y = Deleta a linha. */ 
+
+
+/* Versionamento
+
